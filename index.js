4774910bf7126b6b2292acf58910192f8f2c1a4c
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-14 11:30:20 +0600

    create a new api for services

--- index.js
+++ index.js
@@ -10,12 +10,28 @@ app.use(express.json());
 
 // this code from mongo db
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.brclz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database connected");
+
+    const serviceCollection = client.db("doctor_portal").collection("services");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 // this is from mongodb
 
 app.get("/", (req, res) => {
